031ce1675fe1301796044c92570cd6e7d8199898
Yulingsong <yulingsong5@126.com>
2016-01-15 11:33:35 +0800

    Initial Commit

---
+++ tableviewtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableviewtest
+//
+//  Created by yulingsong on 16/1/15.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
